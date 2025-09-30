06982445b59c15d3f9b25deabc4074d77dd31f53
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-09-30 20:04:03 -0300

    Update DadoColetado.java

--- DadoColetado.java
+++ DadoColetado.java
@@ -1,1 +1,32 @@
+// incompleto
 
+public class DadoColetado {
+
+  private int id;
+  private int fkMissao; // Chave estrangeira no modelo OO, para referência à Missão
+  private String imagemURL;
+  private double temperatura; 
+  private double umidade;    
+  private double valorSensorSimulado;
+
+  public DadoColetado() {
+  }
+
+  public void registrarDado() {
+  }
+
+
+
+  // Getters
+  public int getId() {
+      return id;
+  }
+
+  public int getFkMissao() {
+      return fkMissao;
+  }
+
+  public double getTemperatura() {
+      return temperatura;
+  }
+}
