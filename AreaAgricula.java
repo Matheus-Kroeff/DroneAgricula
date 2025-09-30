1c9f9c2dc8294ef9e89790a7f41e7ea8177fd51c
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-09-30 19:39:27 -0300

    Update AreaAgricula.java

--- AreaAgricula.java
+++ AreaAgricula.java
@@ -1,1 +1,37 @@
+public class AreaAgricola {
 
+    private int id;
+    private double tamanhoHectares;
+    private String localizacao;
+    private String tipoCultivo;
+
+    // Construtor
+    public AreaAgricola(int id, double tamanhoHectares, String localizacao, String tipoCultivo) {
+        this.id = id;
+        this.tamanhoHectares = tamanhoHectares;
+        this.localizacao = localizacao;
+        this.tipoCultivo = tipoCultivo;
+    }
+
+    // Getters 
+    public int getId() {
+        return id;
+    }
+
+    public double getTamanhoHectares() {
+        return tamanhoHectares;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public String getTipoCultivo() {
+        return tipoCultivo;
+    }
+
+    // Método de cadastro (simulação)
+    public void cadastrarArea() {
+        System.out.println("Área Agrícola ID " + this.id + " (" + this.tipoCultivo + ") cadastrada com sucesso.");
+    }
+}
