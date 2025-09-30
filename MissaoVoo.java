09550cb4771fd9d597e5513d3f740f6480cecb33
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-09-30 19:59:11 -0300

    Update MissaoVoo.java

--- MissaoVoo.java
+++ MissaoVoo.java
@@ -1,1 +1,36 @@
+// Incompleto
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public Class MissaoVoo {
+
+  private int id;
+  private LocalDate data;
+  private LocalTime horarioInicio;
+  private LocalTime horarioFim;
+
+  private Drone droneUtilizado;
+  private AreaAgricola areaVoo;
+  private Usuario operador; // O operador que agendou a missão
+
+  public MissaoVoo() }
+    }
+
+  public boolean validarNaoSobreposta() {
+    }
+
+
+
+
+// Getters
+  public int getId() {
+        return id;
+  }
+
+  public Drone getDroneUtilizado() {
+       return droneUtilizado;
+  }
+}
