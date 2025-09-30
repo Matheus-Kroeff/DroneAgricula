168b04576607b33d989e6d02b9b5fff87d5637b7
Matheus Kroeff <matheuslckroeff@gmail.com>
2025-09-30 19:50:31 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,43 @@
+// Incompleto
 
+public class Usuario { 
+
+   public static final String TIPO_ADMIN = "Administrador";
+   public static final String TIPO_OPERADOR = "Operador";
+
+   private int id;
+   private String nome;
+   private String email;
+   private String tipoAcesso; // Usará as constantes acima
+
+   public Usuario(int id, String nome, String email, String tipoAcesso) {
+      this.id = id;
+      this.nome = nome;
+      this.email = email;
+      this.tipoAcesso = tipoAcesso;
+   }
+
+   public boolean login() {
+   }
+
+   public boolean isAdministrador() {
+       return 
+   }
+
+   public boolean isOperador() {
+        return
+   }
+
+// Getters 
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipoAcesso() {
+        return tipoAcesso;
+    }
+}
